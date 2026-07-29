a9f0c3f6a990f3d9147cf75e9025670311baf53f
Ayush8481Lab <wonder945177@gmail.com>
2026-07-29 20:14:50 +0530

    Update meta.js

--- api/meta.js
+++ api/meta.js
@@ -111,7 +111,7 @@ export default async function handler(req, res) {
       // The complete list of supported languages
       const ALL_LANGUAGES = [
         "hindi", "tamil", "kannada", "telugu", "malayalam", 
-        "marathi", "bengali", "english", "bhojpuri"
+        "marathi", "bengali", "english", "bhojpuri","haryanvi"
       ];
 
       let genreTitle = genres;
